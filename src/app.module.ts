47ab6ee028616030310dd6fb08ea7b4d5a784e68
Carlos Henrique <carlossouzap@outlook.com>
2023-06-26 23:14:49 -0300

    Setting app module to recognize the new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseAsyncConfig } from './config/mongo.config';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GqlConfigService } from './config/graphql.config';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseAsyncConfig,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
