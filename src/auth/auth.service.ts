63d713ef87543a0b6595d35d49d22a34686eddb9
Carlos Henrique <carlossouzap@outlook.com>
2023-08-18 19:44:06 -0300

    Fixing login mutation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,15 +5,18 @@ import { JwtService } from '@nestjs/jwt';
 import { LoginUserInput } from './entities/auth.entity';
 
 import * as bcrypt from 'bcrypt';
+import { CreateUserInput, User } from 'src/user/entities/user.entity';
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private jwtService: JwtService,
+    
   ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findOneByEmail(email);
+    if(!user) return null
     const valid = await bcrypt.compare(password, user?.password);
 
     if (user && valid) {
@@ -25,11 +28,27 @@ export class AuthService {
   }
 
   async login(user: LoginUserInput): Promise<any> {
-    return {
-      access_token: this.jwtService.sign({
+
+    if(await this.validateUser(user.email, user.password )){
+      return {
+        access_token: this.jwtService.sign({
+          email: user.email,
+        }),
         email: user.email,
-      }),
-      email: user.email,
-    };
+      };
+    }
+    else{
+      throw new Error("Usuário ou senha incorreta!")
+    }
+  }
+
+
+  async signUp(user: CreateUserInput): Promise<User>{
+    const userExists = await this.userService.findOneByEmail(
+      user.email,
+    );
+    if (userExists) throw new Error('User already exists!');
+    const password = await bcrypt.hash(user.password, 10);
+    return this.userService.createUser({ ...user, password });
   }
 }
